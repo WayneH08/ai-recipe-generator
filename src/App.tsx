23a54553d2484c0ce9a58cc12f0e5f111218d1dd
Wayne Halliburton <whalliburton18@gmail.com>
2025-04-11 15:00:13 -0500

    adding in my name to the UI

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
     <div className="app-container">
       <div className="header-container">
         <h1 className="main-header">
-          Meet Your Personal
+          Wayne Halliburton Meet Your Personal
           <br />
           <span className="highlight">Recipe AI</span>
         </h1>
